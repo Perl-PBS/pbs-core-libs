0ce0d3eeb6d4ae3f73ec4d65b885c5e15f2ed27d
Pedro Melo <melo@simplicidade.org>
2014-08-19 22:26:59 +0100

    Added experimental 'switch' to our Std imports
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/PBS/Setup/Std.pm
+++ lib/PBS/Setup/Std.pm
@@ -3,16 +3,18 @@ package PBS::Setup::Std;
 # ABSTRACT: the most basic Perl setup environment
 
 use 5.014;
-use strict   ();
-use warnings ();
-use utf8     ();
-use feature  ();
+use strict       ();
+use warnings     ();
+use utf8         ();
+use feature      ();
+use experimental ();
 
 sub import {
   warnings->import();
   strict->import();
   utf8->import();
   feature->import(':5.14');
+  experimental->import('switch');
 }
 
 1;
