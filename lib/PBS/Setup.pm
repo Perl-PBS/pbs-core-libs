5e832e980c630769abcf4ca88c7964fd496b1fce
Pedro Melo <melo@simplicidade.org>
2014-08-20 16:08:49 +0100

    Update env() on ::Setup to support "auto" environment, the new default:
    If no PROJECT_ENV or :env is defined, we now try to figure out the
    PROJECT_ENV to set using this rules:
    
    * if <project_root>/.git exists, set env as "devel";
    * if <project_root>/.deployed_env exists, read it and use the first line
      as our env.
    
    If none match, the process dies.
    
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/PBS/Setup.pm
+++ lib/PBS/Setup.pm
@@ -25,9 +25,22 @@ sub std_imports { goto \&PBS::Setup::Std::import }
   sub env {
     return $env if defined $env;
 
+    my ($class, $requested_default_env) = @_;
+
     return $env = do {
-      my $v = $ENV{PROJECT_ENV} || $_[1];
-      Carp::confess("FATAL: PROJECT_ENV is required") unless defined $v;
+      my $v = $ENV{PROJECT_ENV} || $requested_default_env || 'auto';
+
+      if ($v eq 'auto') {
+        if (-d $class->root_dir('.git')) { $v = 'devel' }
+        elsif (-e (my $dep_env_fn = $class->root_dir('.deployed_env'))) {
+          open(my $fh, '<', $dep_env_fn);
+          chomp($v = <$fh>);
+        }
+
+        Carp::confess("FATAL: PROJECT_ENV is required, and could not be automatically determined")
+          unless $v and $v ne 'auto';
+        warn "PROJECT_ENV set to '$v' automatically\n" unless $ENV{SKIP_AUTO_PROJECT_ENV_WARNING};
+      }
 
       $v =~ s/^://;
 
